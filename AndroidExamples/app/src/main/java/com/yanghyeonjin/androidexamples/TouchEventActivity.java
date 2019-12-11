b5299afbe39a0faf6314a707f471bf599a69835b
yanghyeonjin <guswls7081@naver.com>
2019-12-11 14:07:17 +0900

    Do it - GestureDetector

--- AndroidExamples/app/src/main/java/com/yanghyeonjin/androidexamples/TouchEventActivity.java
+++ AndroidExamples/app/src/main/java/com/yanghyeonjin/androidexamples/TouchEventActivity.java
@@ -3,14 +3,17 @@ package com.yanghyeonjin.androidexamples;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class TouchEventActivity extends AppCompatActivity {
 
-    private TextView tvTouchResult;
-    private View vTouchEvent, v2;
+    private TextView tvTouchResult, tvGestureResult;
+    private View vTouchEvent, vGestureEvent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +22,49 @@ public class TouchEventActivity extends AppCompatActivity {
 
         /* 아이디 연결 */
         tvTouchResult = findViewById(R.id.tv_touch_result);
+        tvGestureResult = findViewById(R.id.tv_gesture_result);
         vTouchEvent = findViewById(R.id.v_touch_event);
+        vGestureEvent = findViewById(R.id.v_gesture_event);
+
+        /* GestureDetector 객체 선언 */
+        GestureDetector gestureDetector;
+        gestureDetector = new GestureDetector(this, new GestureDetector.OnGestureListener() {
+            @Override
+            public boolean onDown(MotionEvent motionEvent) {
+                println2("onDown() 호출됨.");
+                return true;
+            }
+
+            @Override
+            public void onShowPress(MotionEvent motionEvent) {
+                println2("onShowPress() 호출됨.");
+            }
+
+            @Override
+            public boolean onSingleTapUp(MotionEvent motionEvent) {
+                println2("onSingleTapUp() 호출됨.");
+                return true;
+            }
+
+            /* 손가락으로 드래그하는 일반적인 경우. 이동한 거리값이 중요하게 처리됨 */
+            @Override
+            public boolean onScroll(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
+                println2("onScroll() 호출됨 : " + v + ", " + v1);
+                return true;
+            }
+
+            @Override
+            public void onLongPress(MotionEvent motionEvent) {
+                println2("onLongPress() 호출됨.");
+            }
+
+            /* 빠른 속도로 스크롤을 하는 경우. 이동한 속도값이 중요하게 처리됨 */
+            @Override
+            public boolean onFling(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
+                println2("onFling() 호출됨.");
+                return true;
+            }
+        });
 
         /* 뷰를 터치했을 때 이벤트를 전달받을 리스너 등록*/
         vTouchEvent.setOnTouchListener(new View.OnTouchListener() {
@@ -47,9 +92,34 @@ public class TouchEventActivity extends AppCompatActivity {
                 return true;
             }
         });
+
+        /* 뷰를 터치했을 때 발생하는 터치 이벤트를 제스처 디텍터로 전달 */
+        vGestureEvent.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                gestureDetector.onTouchEvent(motionEvent);
+                return true;
+            }
+        });
     }
 
     private void println(String str) {
         tvTouchResult.append(str + "\n");
+    }
+
+    private void println2(String str) {
+        tvGestureResult.append(str + "\n");
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        /* 시스템 [BACK] 버튼이 눌렀을 경우 토스트 메시지 보여주기 */
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            Toast.makeText(this, "시스템 [BACK] 버튼이 눌렸습니다.", Toast.LENGTH_LONG).show();
+            super.onKeyDown(keyCode, event);
+            return true;
+        }
+
+        return false;
     }
 }
