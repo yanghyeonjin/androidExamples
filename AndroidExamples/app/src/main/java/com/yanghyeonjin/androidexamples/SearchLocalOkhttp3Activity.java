abf2aecd3680011a8049ee1860549c8174fdcc16
yanghyeonjin <guswls7081@naver.com>
2019-12-16 14:37:17 +0900

    okhttp3 카카오 로컬, 네이버 검색 response 값 받아와서 핸들러를 이용하여 view에 표시

--- AndroidExamples/app/src/main/java/com/yanghyeonjin/androidexamples/SearchLocalOkhttp3Activity.java
+++ AndroidExamples/app/src/main/java/com/yanghyeonjin/androidexamples/SearchLocalOkhttp3Activity.java
@@ -1,8 +1,17 @@
 package com.yanghyeonjin.androidexamples;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.gson.Gson;
@@ -20,22 +29,117 @@ import okhttp3.Response;
 
 public class SearchLocalOkhttp3Activity extends AppCompatActivity {
 
-    private String kakaoLocalAppKey;
+    private String kakaoLocalAppKey, naverClientID, naverClientSecret;
+
+    private EditText etKakaoSearchLocal, etNaverSearchLocal;
+    private Button btnKakaoSearchLocal, btnNaverSearchLocal;
+    private TextView tvKakaoResultName, tvKakaoResultAddress, tvKakaoResultRoadAddress, tvKakaoResultTel;
+    private TextView tvNaverResultName, tvNaverResultAddress, tvNaverResultRoadAddress, tvNaverResultTel;
+
+    private OkHttpClient kakaoHttpClient, naverHttpClient;
+
+    private KakaoLocalKeywordResult kakaoResult;
+    private NaverSearchLocalResult naverResult;
+
+    private Handler kakaoHandler, naverHandler;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_kakao_local);
+        setContentView(R.layout.activity_search_local);
 
         kakaoLocalAppKey = getString(R.string.kakao_local_app_key);
+        naverClientID = getString(R.string.naver_client_id);
+        naverClientSecret = getString(R.string.naver_client_secret);
+
+
+        /* 아이디 연결 */
+        etKakaoSearchLocal = findViewById(R.id.et_kakao_search_local);
+        etNaverSearchLocal = findViewById(R.id.et_naver_search_local);
+
+        btnKakaoSearchLocal = findViewById(R.id.btn_kakao_search_local);
+        btnNaverSearchLocal = findViewById(R.id.btn_naver_search_local);
+
+        tvKakaoResultName = findViewById(R.id.tv_kakao_result_name);
+        tvKakaoResultAddress = findViewById(R.id.tv_kakao_result_address);
+        tvKakaoResultRoadAddress = findViewById(R.id.tv_kakao_result_road_address);
+        tvKakaoResultTel = findViewById(R.id.tv_kakao_result_tel);
+
+        tvNaverResultName = findViewById(R.id.tv_naver_result_name);
+        tvNaverResultAddress = findViewById(R.id.tv_naver_result_address);
+        tvNaverResultRoadAddress = findViewById(R.id.tv_naver_result_road_address);
+        tvNaverResultTel = findViewById(R.id.tv_naver_result_tel);
+
+
+        /* 검색어 없을 때 나타나는 AlertDialog 초기화 */
+        AlertDialog.Builder builder = initNoKeyword();
 
 
 
-        /* 카카오 */
-        OkHttpClient kakaoHttpClient = new OkHttpClient();
+        /* okhttp3 초기화 */
+        kakaoHttpClient = new OkHttpClient();
+        naverHttpClient = new OkHttpClient();
 
+
+        /* 핸들러 초기화 */
+        kakaoHandler = new Handler(Looper.getMainLooper());
+        naverHandler = new Handler(Looper.getMainLooper());
+
+
+
+        /* 카카오 검색 눌렀을 때 */
+        btnKakaoSearchLocal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String kakaoKeyword = etKakaoSearchLocal.getText().toString();
+
+                if (kakaoKeyword.isEmpty()) {
+                    /* 검색창 빈칸이면 */
+                    builder.show();
+                } else {
+                    /* 빈칸 아니면 검색 시작 */
+                    searchKakaoLocal(kakaoKeyword);
+                }
+            }
+        });
+
+        /* 네이버 검색 눌렀을 때 */
+        btnNaverSearchLocal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String naverKeyword = etNaverSearchLocal.getText().toString();
+
+                if (naverKeyword.isEmpty()) {
+                    /* 검색창 빈칸이면 */
+                    builder.show();
+                } else {
+                    /* 빈칸 아니면 검색 시작 */
+                    searchNaverLocal(naverKeyword);
+                }
+            }
+        });
+    }
+
+    private AlertDialog.Builder initNoKeyword() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("검색어 없음");
+        builder.setMessage("검색어를 입력해주세요.");
+
+        builder.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+
+        return builder;
+    }
+
+    /* 카카오 검색 */
+    private void searchKakaoLocal(String keyword) {
         HttpUrl.Builder kakaoUrlBuilder = HttpUrl.parse("https://dapi.kakao.com/v2/local/search/keyword.json").newBuilder();
-        kakaoUrlBuilder.addEncodedQueryParameter("query", "파스타");
+        kakaoUrlBuilder.addEncodedQueryParameter("query", keyword);
         String kakaoRequestUrl = kakaoUrlBuilder.build().toString();
 
         Request kakaoRequest = new Request.Builder()
@@ -46,66 +150,84 @@ public class SearchLocalOkhttp3Activity extends AppCompatActivity {
         kakaoHttpClient.newCall(kakaoRequest).enqueue(new Callback() {
             @Override
             public void onFailure(Call call, IOException e) {
-                Log.e("kakao: onFailure", "error + Connect Server Error is " + e.toString());
+                Log.e("kakao search", e.getMessage());
             }
 
             @Override
             public void onResponse(Call call, Response response) throws IOException {
                 if (response.body() != null) {
-                    /* response.body().string()은 한번만 호출하자. */
                     String result = response.body().string();
 
                     Gson gson = new Gson();
-                    KakaoLocalKeywordResult kakaoLocalKeywordResult = gson.fromJson(result, KakaoLocalKeywordResult.class);
+                    kakaoResult = gson.fromJson(result, KakaoLocalKeywordResult.class);
 
-                    Log.e("placeName", kakaoLocalKeywordResult.getDocuments().get(0).getPlaceName());
-                    Log.e("address", kakaoLocalKeywordResult.getDocuments().get(0).getAddressName());
-                    Log.e("roadAddress", kakaoLocalKeywordResult.getDocuments().get(0).getRoadAddressName());
+                    kakaoHandler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            String placeName = kakaoResult.getDocuments().get(0).getPlaceName();
+                            String address = kakaoResult.getDocuments().get(0).getAddressName();
+                            String roadAddress = kakaoResult.getDocuments().get(0).getRoadAddressName();
+                            String tel = kakaoResult.getDocuments().get(0).getPhone();
+
+                            tvKakaoResultName.setText(placeName);
+                            tvKakaoResultAddress.setText(address);
+                            tvKakaoResultRoadAddress.setText(roadAddress);
+                            tvKakaoResultTel.setText(tel);
+
+                        }
+                    });
+
                 } else {
-                    Log.e("kakao: onResponse", "Response Body is null");
+                    Log.e("kakao search", "Response Body is null");
                 }
             }
         });
+    }
 
-
-        /* 네이버 */
-        final String naverClientID = getString(R.string.naver_client_id);
-        final String naverClientSecret = getString(R.string.naver_client_secret);
-
-        OkHttpClient naverHttpClient = new OkHttpClient();
-
+    /* 네이버 검색 */
+    private void searchNaverLocal(String keyword) {
         HttpUrl.Builder naverUrlBuilder = HttpUrl.parse("https://openapi.naver.com/v1/search/local.json").newBuilder();
-        naverUrlBuilder.addEncodedQueryParameter("query", "파스타");
+        naverUrlBuilder.addEncodedQueryParameter("query", keyword);
         String naverRequestUrl = naverUrlBuilder.build().toString();
 
-        Request request = new Request.Builder()
+        Request naverRequest = new Request.Builder()
                 .addHeader("X-Naver-Client-Id", naverClientID)
                 .addHeader("X-Naver-Client-Secret", naverClientSecret)
                 .url(naverRequestUrl)
                 .build();
 
-        naverHttpClient.newCall(request).enqueue(new Callback() {
+        kakaoHttpClient.newCall(naverRequest).enqueue(new Callback() {
             @Override
             public void onFailure(Call call, IOException e) {
-                Log.e("Naver: onFailure", "error + Connect Server Error is " + e.toString());
+                Log.e("naver search", e.getMessage());
             }
 
             @Override
             public void onResponse(Call call, Response response) throws IOException {
                 if (response.body() != null) {
-                    /* response.body().string()은 한번만 호출하자. */
                     String result = response.body().string();
 
                     Gson gson = new Gson();
-                    NaverSearchLocalResult naverSearchLocalResult = gson.fromJson(result, NaverSearchLocalResult.class);
+                    naverResult = gson.fromJson(result, NaverSearchLocalResult.class);
 
-                    Log.e("title", naverSearchLocalResult.getItems().get(0).getTitle());
-                    Log.e("address", naverSearchLocalResult.getItems().get(0).getAddress());
-                    Log.e("roadAddress", naverSearchLocalResult.getItems().get(0).getRoadAddress());
+                    naverHandler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            String placeName = naverResult.getItems().get(0).getTitle();
+                            String address = naverResult.getItems().get(0).getAddress();
+                            String roadAddress = naverResult.getItems().get(0).getRoadAddress();
+                            String tel = naverResult.getItems().get(0).getTelephone();
+
+                            tvNaverResultName.setText(placeName);
+                            tvNaverResultAddress.setText(address);
+                            tvNaverResultRoadAddress.setText(roadAddress);
+                            tvNaverResultTel.setText(tel);
+                        }
+                    });
+
                 } else {
-                    Log.e("Naver: onResponse", "Response Body is null");
+                    Log.e("naver search", "Response Body is null");
                 }
-
             }
         });
     }
